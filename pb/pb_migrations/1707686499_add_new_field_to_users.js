f5d4fc46696ec3f95843a2fb6527cd54290d3dce
DrMxrcy <drmxrcy@gmail.com>
2024-10-15 21:14:22 -0400

    Update 1707686499_add_new_field_to_users.js

--- pb/pb_migrations/1707686499_add_new_field_to_users.js
+++ pb/pb_migrations/1707686499_add_new_field_to_users.js
@@ -1,21 +1,20 @@
-module.exports = {
-    up: async (db) => {
-        await db.collection('users').updateSchema({
-            fields: [
-                { 
-                    name: 'webID', 
-                    type: 'number', 
-                    required: true, 
-                    options: { min: 1 } // Ensures the number is non-zero
-                }
-            ]
-        });
-    },
-    down: async (db) => {
-        await db.collection('users').updateSchema({
-            fields: [
-                // Remove the webID field
-            ]
-        });
-    }
-};
+export async function up(db) {
+    await db.collection('users').updateSchema({
+        fields: [
+            { 
+                name: 'webID', 
+                type: 'number', 
+                required: true, 
+                options: { min: 1 } // Ensures the number is non-zero
+            }
+        ]
+    });
+}
+
+export async function down(db) {
+    await db.collection('users').updateSchema({
+        fields: [
+            // Remove the webID field
+        ]
+    });
+}
