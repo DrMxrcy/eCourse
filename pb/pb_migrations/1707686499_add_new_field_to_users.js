ec3c07757fb69393e9f363e02cbcfc5e9d411d30
DrMxrcy <drmxrcy@gmail.com>
2024-10-15 21:15:14 -0400

    Update 1707686499_add_new_field_to_users.js

--- pb/pb_migrations/1707686499_add_new_field_to_users.js
+++ pb/pb_migrations/1707686499_add_new_field_to_users.js
@@ -1,20 +1,29 @@
-export async function up(db) {
-    await db.collection('users').updateSchema({
-        fields: [
-            { 
-                name: 'webID', 
-                type: 'number', 
-                required: true, 
-                options: { min: 1 } // Ensures the number is non-zero
-            }
-        ]
-    });
-}
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+    const dao = new Dao(db);
+    const collection = dao.findCollectionByNameOrId("_pb_users_auth_");
 
-export async function down(db) {
-    await db.collection('users').updateSchema({
-        fields: [
-            // Remove the webID field
-        ]
-    });
-}
+    // Add the new field
+    collection.schema.addField(new SchemaField({
+        system: false,
+        id: "webID",
+        name: "webID",
+        type: "number",
+        required: true,
+        presentable: false,
+        unique: false,
+        options: {
+            min: 1 // Ensures the number is non-zero
+        }
+    }));
+
+    return dao.saveCollection(collection);
+}, (db) => {
+    const dao = new Dao(db);
+    const collection = dao.findCollectionByNameOrId("_pb_users_auth_");
+
+    // Remove the webID field
+    collection.schema.removeField("webID");
+
+    return dao.saveCollection(collection);
+});
